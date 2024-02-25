8c02335553780b04c32b8e4824480fded62aad01
Matthias Vitt <matthias.vitt@gmail.com>
2024-02-25 16:42:35 +0100

    added test

--- src/buildpy3/app.py
+++ src/buildpy3/app.py
@@ -84,8 +84,9 @@ class Webhook(Resource):
 
         payload = request.data
 
-        # Verify the signature
+        # Verify the signature 
         if not verify_signature(payload, signature):
+            logging.warn("Invalid signature")
             return jsonify({'message': 'Invalid signature'}), 403
 
         # Process the GitHub event here (e.g., push, pull request)
